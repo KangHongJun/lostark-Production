ac05ce200f5baa15b1e5721951b915c42ad31a31
KangHongJun <40516203+KangHongJun@users.noreply.github.com>
2021-06-16 10:32:25 +0900

    Create lostark.py
    sample

---
+++ lostark.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://lostark.game.onstove.com/Market/List_v2?firstCategory=90000&secondCategory=90400&characterClass=&tier=0&grade=99&itemName=&pageNo=1&isInit=false&sortType=7&_=1623805762400'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+    title = soup.select_one('#tbodyItemList > tr:nth-child(4) > td:nth-child(1) > div > span.slot')
+    print(title)
+else : 
+    print(response.status_code)
+
+
+# In[ ]:
+
+
+
+
+
+# In[3]:
+
+
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://lostark.game.onstove.com/Market/List_v2?firstCategory=90000&secondCategory=90400&characterClass=&tier=0&grade=99&itemName=&pageNo=1&isInit=false&sortType=7&_=1623805762400'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+    title = soup.select_one('#tbodyItemList > tr:nth-child(4) > td:nth-child(2) > div > em')
+    print(title)
+else : 
+    print(response.status_code)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
