fab04010dd8b1fb934ebbe2cd3b0ea52b938a11d
KangHongJun <40516203+KangHongJun@users.noreply.github.com>
2021-07-20 15:31:59 +0900

    Update Plants_list.py
    Reorganization

--- Plants_list.py
+++ Plants_list.py
@@ -11,9 +11,34 @@ def Get_price():
     if response.status_code == 200:
         html = response.text
         soup = BeautifulSoup(html, 'html.parser')
-        #데이터 하나씩 뽑아와야함
-        title = soup.select_one('#tbodyItemList > tr:nth-child(9) > td:nth-child(4) > div > em')
-        print(title)
+       
+    
+    #들꽃 
+    Flower = soup.select_one('#tbodyItemList > tr:nth-child(6) > td:nth-child(4) > div > em')
+    int(Flower.text)
+    
+    #수줍은 들꽃 
+    Shy_flower = soup.select_one('#tbodyItemList > tr:nth-child(2) > td:nth-child(4) > div > em')
+    int(Shy_flower.text)
+    
+    #화사한 들꽃 
+    Bright_flower = soup.select_one('#tbodyItemList > tr:nth-child(5) > td:nth-child(4) > div > em')
+    int(Bright_flower.text)
+    
+    #투박한 버섯 
+    Coarse_mushroom = soup.select_one('#tbodyItemList > tr:nth-child(3) > td:nth-child(4) > div > em')
+    int(Coarse_mushroom.text)
+    
+    #싱싱한 버섯 
+    Fresh_mushroom = soup.select_one('#tbodyItemList > tr:nth-child(1) > td:nth-child(4) > div > em')
+    int(Fresh_mushroom.text)
+    
+    #화려한 버섯 
+    Fancy_mushroom = soup.select_one('#tbodyItemList > tr:nth-child(4) > td:nth-child(4) > div > em')
+    int(Fancy_mushroom.text)
+
+       
+    
     else:
         print(response.status_code)
         print('sd')
