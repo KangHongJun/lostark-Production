4ffbe0a0c5103f0ecb17dd039149b34736469e40
KangHongJun <40516203+KangHongJun@users.noreply.github.com>
2021-07-20 15:15:43 +0900

    Update Hunting_list.py
    Reorganization

--- Hunting_list.py
+++ Hunting_list.py
@@ -11,9 +11,33 @@ def Get_price():
     if response.status_code == 200:
         html = response.text
         soup = BeautifulSoup(html, 'html.parser')
-        #데이터 하나씩 뽑아와야함
-        title = soup.select_one('#tbodyItemList > tr:nth-child(9) > td:nth-child(4) > div > em')
-        print(title)
+       
+    
+        #다듬은 생고기 
+        Meat = soup.select_one('#tbodyItemList > tr:nth-child(1) > td:nth-child(4) > div > em')
+        Meat = int(Meat)
+        
+        #칼다르 두툼한 생고기 
+        K_Meat= soup.select_one('#tbodyItemList > tr:nth-child(2) > td:nth-child(4) > div > em')
+        K_Meat = int(K_Meat)
+        
+        #오레하 두툼한 생고기 
+        O_Meat= soup.select_one('#tbodyItemList > tr:nth-child(4) > td:nth-child(4) > div > em')
+        O_Meat = int(O_Meat)
+        
+        #두툼한 생고기(100) 
+        Thick_Meat= soup.select_one('#tbodyItemList > tr:nth-child(6) > td:nth-child(4) > div > em')
+        Thick_Meat = int(Thick_Meat)
+
+        #질긴가죽 
+        Tough_Leather= soup.select_one('#tbodyItemList > tr:nth-child(3) > td:nth-child(4) > div > em')
+        Tough_Leather = int(Tough_Leather)
+        
+        #수렵 결정 (5)
+
+
+       
+        print(Meat)
     else:
         print(response.status_code)
         print('sd')
