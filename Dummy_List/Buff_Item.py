5861e29f446afa8412fdc96ab01405d090b1adb7
KangHongJun <40516203+KangHongJun@users.noreply.github.com>
2021-08-17 14:15:40 +0900

    Create Buff_Item.py

---
+++ Dummy_List/Buff_Item.py
@@ -0,0 +1,36 @@
+#전투보조
+import requests
+from bs4 import BeautifulSoup
+
+def Assistance:
+    A_url='https://lostark.game.onstove.com/Market/List_v2?firstCategory=60000&secondCategory=60000&characterClass=&tier=0&grade=99&itemName=&pageNo=1&isInit=false&sortType=1&_=1627911349466'
+   
+    response = requests.get(A_url)
+    
+    html = response1.text
+    
+    soup = BeautifulSoup(html, 'html.parser')
+   
+    #1page
+    
+    #보호 물약
+    
+    #진군의 깃발
+    
+    #신속 로브
+    
+    #각성 물약
+    
+    #아드로핀 물약
+    
+    #빛나는 보호 물약
+    
+    #빛나는 진군의 깃발
+    
+    #빛나는 신속 
+    
+    
+    
+    
+    
+    
