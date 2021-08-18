d4402a4a0d618e21ae2783ea77ac22aeec907a51
KangHongJun <40516203+KangHongJun@users.noreply.github.com>
2021-08-18 09:36:50 +0900

    Update potion.py

--- Dummy_List/potion.py
+++ Dummy_List/potion.py
@@ -19,11 +19,11 @@ def Potion: #이후 수정
     Rare_Healing = int(Rare_Healing.text)
 
     #정령의 회복약
-    Spirit_Healing = Spirit_Healing.select_one('#tbodyItemList > tr:nth-child(3) > td:nth-child(4) > div > em')
+    Spirit_Healing = soup.select_one('#tbodyItemList > tr:nth-child(3) > td:nth-child(4) > div > em')
     Spirit_Healing = int(Spirit_Healing.text)
 
     #빛나는 정령의 회복약
-    S_Spirit_Healing = S_Spirit_Healing.select_one('#tbodyItemList > tr:nth-child(4) > td:nth-child(4) > div > em')
+    S_Spirit_Healing = soup.select_one('#tbodyItemList > tr:nth-child(4) > td:nth-child(4) > div > em')
     S_Spirit_Healing = int(S_Spirit_Healing.text)
 
     con = sqlite3.connect(".db")
