6e67077ed253a0c83b04631b0ebc0a4ef3a25568
KangHongJun <40516203+KangHongJun@users.noreply.github.com>
2021-08-24 13:56:09 +0900

    import UI
    The code is repetition, so i make UI module and import it at tap2-Attack_Item 1 item

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from PyQt5.QtCore import *
 import Life_list
 import Attack_Item_list
 
+from UI import Attack_ItemUI
+
 Life_list.Life()
 Attack_Item_list.Attack()
 
@@ -496,17 +498,205 @@ class grid2(QWidget):
 
     def display(self, i):
         self.Stack.setCurrentIndex(i)
+        
     
+class grid(QWidget):
+    def __init__(self):
+        super(grid, self).__init__()
+        self.leftlist = QListWidget()
+        self.leftlist.insertItem(0, '1')
+        self.leftlist.insertItem(1, '2')
+        self.leftlist.insertItem(2, '3')
+        self.leftlist.insertItem(3, '4')
+        
+        self.stack1 = QWidget()
+        self.stack2 = QWidget()
+        self.stack3 = QWidget()
+        self.stack4 = QWidget()
+        
+        self.Healing()
+        self.Rare_Healing()
+        self.Spirit_Healing()
+        self.S_Spirit_Healing()
+
+        self.Stack = QStackedWidget(self)
+        self.Stack.addWidget(self.stack1)
+        self.Stack.addWidget(self.stack2)
+        self.Stack.addWidget(self.stack3)
+        self.Stack.addWidget(self.stack4)
+
+        hbox = QHBoxLayout(self)
+        hbox.addWidget(self.leftlist)
+        hbox.addWidget(self.Stack)
+
+        self.setLayout(hbox)
+        self.leftlist.currentRowChanged.connect(self.display)
+        self.setGeometry(300, 50, 10, 10)
+        self.setWindowTitle('StackedWidget demo')
+        self.show()
     
-    #수수료    
-    def lifting(value):
-      if(value == 1):
-        return 0
-      if ((value*0.05)%10 != 0):
-        value = int(value*0.05) + 1
-        return value
-      if ((value*0.05)%10 == 0):
-        return int(value*0.05)
+    #회복약
+    def Healing(self):
+        layout = QFormLayout()
+
+        label1 = QLabel()
+        label1.setText("회복약\n")
+
+        label2 = QLabel()
+        label2.setText("수줍은 들꽃x5 - " + "골드")
+
+        label3 = QLabel()
+        label3.setText("들꽃x10")
+
+        label4 = QLabel()
+        label4.setText('실링 x1200')
+        label4.setText(str(df_list[0][2]))
+
+        label5 = QLabel()
+        label5.setText("0골드")
+        
+        profit = QLabel()
+        profit.setText(Q)
+
+        str(df_list[0][2])
+
+        
+        #label.move(20, 20) 형식으로 제작템 가격 - 수수료 계산
+
+        layout.addWidget(label1)
+        layout.addWidget(label2)
+        layout.addWidget(label3)
+        layout.addWidget(label4)
+        layout.addWidget(label5)
+        layout.addWidget(profit)
+
+        self.stack1.setLayout(layout)
+        
+    #고급 회복약
+    def Rare_Healing (self):
+        layout = QFormLayout()
+
+        label1 = QLabel()
+        label1.setText("고급 회복약\n")
+
+        label2 = QLabel()
+        label2.setText("수줍은 들꽃x9 - " + "골드")
+
+        label3 = QLabel()
+        label3.setText("들꽃x18")
+
+        label4 = QLabel()
+        label4.setText("15골드")
+        
+        profit = QLabel()
+        profit.setText("제작템 값*수수료 계산" + "재료값" +"= 이익")
+
+        
+        #label.move(20, 20) 형식으로 제작템 가격 - 수수료 계산
+        layout.addWidget(label1)
+        layout.addWidget(label2)
+        layout.addWidget(label3)
+        layout.addWidget(label4)
+        layout.addWidget(profit)
+
+        self.stack1.setLayout(layout)
+
+
+
+    def Spirit_Healing(self):
+        layout = QFormLayout()
+
+        label1 = QLabel()
+        label1.setText("정령의 회복약\n")
+
+        label2 = QLabel()
+        label2.setText("화사한 들꽃x6 - " + "골드")
+
+        label3 = QLabel()
+        label3.setText("수줍은 들꽃x24")
+
+        label4 = QLabel()
+        label4.setText('들꽃x48')
+
+        label5 = QLabel()
+        label5.setText("30골드")
+        
+
+
+        
+        #label.move(20, 20) 형식으로 제작템 가격 - 수수료 계산
+        layout.addWidget(label1)
+        layout.addWidget(label2)
+        layout.addWidget(label3)
+        layout.addWidget(label4)
+        layout.addWidget(label5)
+
+
+        self.stack1.setLayout(layout)
+
+        
+    def S_Spirit_Healing(self):
+        layout = QFormLayout()
+
+        label1 = QLabel()
+        label1.setText("고급 정령의 회복약\n")
+
+        label2 = QLabel()
+        label2.setText("정령의 회복약x3 - " + "골드")
+
+        label3 = QLabel()
+        label3.setText("화사한 들꽃x8")
+
+        label4 = QLabel()
+        label4.setText("30골드")
+        
+
+
+        #label.move(20, 20) 형식으로 제작템 가격 - 수수료 계산
+        layout.addWidget(label1)
+        layout.addWidget(label2)
+        layout.addWidget(label3)
+        layout.addWidget(label4)
+
+
+        self.stack1.setLayout(layout)
+
+    def display(self, i):
+        self.Stack.setCurrentIndex(i)
+
+#Attack_Item
+class grid2(QWidget):
+    def __init__(self):
+        super(grid2, self).__init__()
+
+        self.Protection()
+        self.Flag()
+        self.Quick()
+
+        self.Stack = QStackedWidget(self)
+        self.Stack.addWidget(self.stack1)
+        self.Stack.addWidget(self.stack2)
+        self.Stack.addWidget(self.stack3)
+
+        hbox = QHBoxLayout(self)
+        hbox.addWidget(self.leftlist)
+        hbox.addWidget(self.Stack)
+
+        self.setLayout(hbox)
+        self.leftlist.currentRowChanged.connect(self.display)
+        self.setGeometry(300, 50, 10, 10)
+        self.setWindowTitle('StackedWidget demo')
+        self.show()
+        
+        
+    def Flash(self):
+        Attack_ItemUI.Flash()
+        
+
+    
+    def display(self, i):
+        self.Stack.setCurrentIndex(i)    
+   
         
 class MyApp(QMainWindow):
     def __init__(self):
@@ -527,12 +717,27 @@ class MyApp(QMainWindow):
     def make_tap(self):
         # QWidget 적용
         tabs = QTabWidget()
-        tabs.addTab(self.tab1(), '배틀 아이템')
-        tabs.addTab(self.tab1(), '로브')
+        tabs.addTab(self.tab1(), '포션')
+        tabs.addTab(self.tab2(), '전투 아이템')
         self.setCentralWidget(tabs)
 
     def tab1(self):
         wg = grid()
+        self.setCentralWidget(wg)
+
+        hbox = QHBoxLayout()
+        hbox.addWidget(wg)
+
+
+        vbox = QVBoxLayout()
+        vbox.addLayout(hbox)
+
+        tab = QWidget()
+        tab.setLayout(vbox)
+        return tab
+    
+    def tab2(self):
+        wg = grid2()
         self.setCentralWidget(wg)
 
         hbox = QHBoxLayout()
